a2fa3bb5001c4b567f3ac6bd381834b0446ce673
beatriz <beatrizcmpf@gmail.com>
2024-01-17 15:18:52 -0500

    Creation Establishment

---
+++ src/main/java/com/recycle/recycle/controller/EstablishmentController.java
@@ -0,0 +1,79 @@
+package com.recycle.recycle.controller;
+
+import com.recycle.recycle.domain.Establishment;
+import com.recycle.recycle.dto.EstablishmentDTO;
+import com.recycle.recycle.dto.ExceptionDTO;
+import com.recycle.recycle.service.EstablishmentService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController()
+@RequestMapping("/establishment")
+public class EstablishmentController {
+
+    private EstablishmentService establishmentService;
+    private String notFound = "Establishment Not Found!";
+    private String alreadyExist = "Establishment Already Exist";
+    private String deleted = "Establishment deleted successfully!";
+
+    @Autowired
+    public void establishmentController(EstablishmentService establishmentService) {
+        this.establishmentService = establishmentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> registerEstablishment(@Valid @RequestBody EstablishmentDTO establishmentDTO) {
+        try {
+            EstablishmentDTO registeredEstablishment = establishmentService.registerEstablishment(establishmentDTO);
+            return new ResponseEntity<>(registeredEstablishment, HttpStatus.CREATED);
+        } catch (DataIntegrityViolationException ex) {
+            ExceptionDTO exceptionDTO = new ExceptionDTO(alreadyExist, HttpStatus.CONFLICT.value());
+            return ResponseEntity.status(exceptionDTO.statusCode()).body(exceptionDTO);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Establishment>> getAllEstablishment() {
+        List<Establishment> establishments = establishmentService.getAllEstablishment();
+        return new ResponseEntity<>(establishments, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{establishmentId}")
+    public ResponseEntity<?> getEstablishmentById(@PathVariable("establishmentId") String establishmentId) {
+      Optional<Establishment> existingEstablishment = establishmentService.getEstablishmentByID(establishmentId);
+        if (existingEstablishment.isPresent()) {
+            return new ResponseEntity<>(existingEstablishment, HttpStatus.OK);
+        } else {
+            throw new EntityNotFoundException(notFound);
+        }
+    }
+
+    @PutMapping("/{establishmentId}")
+    public ResponseEntity<?> updateEstablishment(@PathVariable("establishmentId") String establishmentId, @RequestBody EstablishmentDTO establishmentDTO) {
+        try {
+            EstablishmentDTO establishment = establishmentService.updateEstablishment(establishmentId, establishmentDTO);
+            return new ResponseEntity<>(establishment, HttpStatus.OK);
+        } catch (DataIntegrityViolationException ex) {
+            ExceptionDTO exception = new ExceptionDTO(notFound, HttpStatus.NOT_FOUND.value());
+            return ResponseEntity.status(exception.statusCode()).body(exception);
+        }
+    }
+
+    @DeleteMapping("/{establishmentId}")
+    public ResponseEntity<?> deleteEstablishment(@PathVariable("establishmentId") String establishmentId) {
+        boolean wasDeleted = establishmentService.deleteEstablishment(establishmentId);
+        if (wasDeleted) {
+            return new ResponseEntity<>(deleted, HttpStatus.OK);
+        }
+        throw new EntityNotFoundException(notFound);
+    }
+}
